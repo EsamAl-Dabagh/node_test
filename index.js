5181f7f09fbe14cd7a6ef11dbf9555b79316e767
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-09-11 09:46:27 +0100

    set up sessions

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
+// require packages
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var session = require('express-session');
 
+//configure app
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(session({
+  secret: "makers makers makers",
+  resave: false,
+  saveUninitialized: true
+}));
+
+//define routes
 app.get("/", function (req, res) {
-  res.send("Hello, World!");
+  res.render("homepage");
 });
 
-app.get("/users/:name/:property", function (req, res) {
-  res.send(req.params);
+app.post("/user/new", function (req, res) {
+  var params = req.body;
+
+  res.redirect("/users");
+});
+
+app.get("/users", function(req, res) {
+  res.render("users");
 });
 
 app.listen(3000, function () {
