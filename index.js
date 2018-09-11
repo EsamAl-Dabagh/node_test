5e619c1ea06a5650bcfb1316c2422417a263da95
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-09-11 11:33:58 +0100

    sessions working

--- index.js
+++ index.js
@@ -2,13 +2,20 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var session = require('express-sessions');
+// var session = require('express-sessions');
+var cookieParser = require("cookie-parser");
+var cookieSession = require("cookie-session");
+
 
 //configure app
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(session({secret: "makers makers makers"}));
+app.use(cookieParser());
+app.use(cookieSession({
+  secret: "makers-makers-makers"
+}));
+// app.use(session({secret: "makers-makers-makers"}));
 
 //define routes
 app.get("/", function (req, res) {
@@ -16,16 +23,16 @@ app.get("/", function (req, res) {
 });
 
 app.post("/user/new", function (req, res) {
-  var params = req.session;
-  params.name = req.params.name;
-  params.property = req.params.property;
+  req.session.name = req.body.name;
+  req.session.property = req.body.property;
 
   res.redirect("/users");
 });
 
-app.get("/users", function(req, res) {
+app.get("/users", function(req, res, next) {
   var name = req.session.name;
   var property = req.session.property;
+  
   res.render("users", {
     name: name,
     property: property
